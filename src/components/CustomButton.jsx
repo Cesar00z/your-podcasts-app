570510d95bde139bf703d6b9c9547069317fb3ec
Cesar00z <cesarbruzual89@gmail.com>
2023-07-03 15:42:28 -0400

    feat: add images and responsive

--- src/components/CustomButton.jsx
+++
@@ -1,7 +0,0 @@
-export default function CustomButton({text}) {
-  return (
-    <button>
-      {text}
-    </button>
-  )
-}
--- src/components/Section1.jsx
+++ src/components/Section1.jsx
@@ -1,6 +1,6 @@
 export default function Section1() {
   return (
-    <section className="section-1">
+    <section className="section-1" id="inicio">
         <div className='image-1'></div>
         <h1>¿Quieres crear tu propio podcast?</h1>
         <p>¡Tenemos la solución perfecta para ti! Ofrecemos un servicio de creación de podcast desde cero, en el que te guiaremos a través de cada paso del proceso.</p>
